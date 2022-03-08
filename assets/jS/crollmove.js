30f5bca7efb797ebecbfd5fe5c39408b5c349955
TCMThang <caominhthangtran@gmail.com>
2022-03-08 17:35:17 +0700

    Update crollmove.js

--- assets/jS/crollmove.js
+++ assets/jS/crollmove.js
@@ -1,28 +1,35 @@
-// $(document).ready(function() {
-
-//     $('.footer-links a').on("click",function(e){e.preventDefault();
-//     //.click(function(event) {
-//     //event.preventDefault();
-    
-//     part = $(this).attr('href'); // lấy id của thẻ h2 tương ứng
-    
-//     position = $(part).offset().top; // tìm vị trí thẻ h2
-    
-//     $('html, body').animate({scrollTop:$(a).offset().top-10},500)})
-//     //.animate({scrollTop: position},1400,'easeInOutSine');
-//     //});
-// });
-
-var $root = $('html, body');
- 
-$('a[href^="#"]').click(function() {
-    var href = $.attr(this, 'href');
- 
-    $root.animate({
-        scrollTop: $(href).offset().top
-    }, 1000, function () {
-        window.location.hash = href;
-    });
- 
-    return false;
-});
+$('a[href*="#"]')
+  // Remove links that don't actually link to anything
+  .not('[href="#"]')
+  .not('[href="#0"]')
+  .click(function(event) {
+    // On-page links
+    if (
+      location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') 
+      && 
+      location.hostname == this.hostname
+    ) {
+      // Figure out element to scroll to
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+      // Does a scroll target exist?
+      if (target.length) {
+        // Only prevent default if animation is actually gonna happen
+        event.preventDefault();
+        $('html, body').animate({
+          scrollTop: target.offset().top
+        }, 1000, function() {
+          // Callback after animation
+          // Must change focus!
+          var $target = $(target);
+          $target.focus();
+          if ($target.is(":focus")) { // Checking if the target was focused
+            return false;
+          } else {
+            $target.attr('tabindex','-1'); // Adding tabindex for elements not focusable
+            $target.focus(); // Set focus again
+          };
+        });
+      }
+    }
+  });
