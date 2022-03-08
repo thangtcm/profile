b9b1c0c2eacc0ad41a6993f39b3e2c062d6c3144
TCMThang <caominhthangtran@gmail.com>
2022-03-08 19:24:41 +0700

    Update test.js

--- assets/jS/test.js
+++ assets/jS/test.js
@@ -1,3 +1,23 @@
-$('[data-spy="scroll"]').each(function () {
-    var $spy = $(this).scrollspy('refresh')
-  })
+// const menu = document.querySelector('.navbar-menu');
+// document.getElementById("scrollspy-nav").style.display = "none";
+// const buttonmenu = document.querySelector('.button-menu');
+
+// function showMenu(){
+//   document.getElementById("scrollspy-nav").style.display = "block";
+// }
+
+// function closeMenu(){
+//   document.getElementById("scrollspy-nav").style.display = "none";
+// }
+
+// buttonmenu.addEventListener('click', showMenu);
+
+
+function myFunction() {
+  var x = document.getElementById("scrollspy-nav");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
